106ffae921ebcc7727948a718458ba35d0897225
yyz <289633152@qq.com>
2015-10-26 05:47:48 -0700

    优化测试代码的输出格式

--- test/bmclient.cpp
+++ test/bmclient.cpp
@@ -4,34 +4,51 @@
 #include <ucorf/server_finder.h>
 #include "demo.rpc.h"
 #include <iostream>
+#include <cstdio>
 #include <boost/smart_ptr/make_shared.hpp>
 using std::cout;
 using std::endl;
 using namespace Echo;
 
+static int concurrecy = 100;
 static std::atomic<size_t> g_count{0};
 static std::atomic<size_t> g_error{0};
+static int g_all_time{0};
+static int g_max_time{0};
 boost_ec last_ec;
 
 void show_status()
 {
+    static int c = 0;
+    if (c++ % 10 == 0) {
+        std::printf("------------------------- Co: %5d ------------------------------\n", concurrecy);
+        std::printf("|   QPS   |  error  | average D | max D | Interval | last_error   \n");
+    }
+
+    static std::chrono::system_clock::time_point last_time = std::chrono::system_clock::now();
+    auto now = std::chrono::system_clock::now();
     static size_t last_count = 0;
     static size_t last_error = 0;
-    size_t ok = g_count - last_count;
-    size_t err = g_error - last_error;
+    int qps = (int)(g_count - last_count);
+    int err = (int)(g_error - last_error);
+    std::string errinfo = last_ec.message();
     last_count = g_count;
     last_error = g_error;
-    cout << "ok: " << ok << endl;
-    cout << "error: " << err << endl;
-    cout << "last_ec: " << last_ec.message() << endl;
     last_ec = boost_ec();
+
+    std::printf("|%7d  |%7d  | %5d     |%5d  | %6d   | %s\n",
+            qps, err, g_all_time / (qps + 1), g_max_time,
+            (int)std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time).count(),
+            errinfo.c_str()
+            );
+    last_time = now;
+    g_all_time = g_max_time = 0;
 }
 
 int main(int argc, char** argv)
 {
     using namespace ucorf;
 
-    int concurrecy = 100;
     if (argc > 1) {
         concurrecy = atoi(argv[1]);
     }
@@ -77,6 +94,7 @@ int main(int argc, char** argv)
                 request.set_code(1);
                 EchoResponse response;
 
+                auto now = std::chrono::system_clock::now();
                 boost_ec ec = stub.Echo(request, &response);
                 if (ec) {
                     co_yield;
@@ -85,6 +103,10 @@ int main(int argc, char** argv)
 //                    cout << "rpc call error: " << ec.message() << endl;
                 } else {
                     ++g_count;
+                    int delay = (int)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - now).count();
+                    g_all_time += delay;
+                    if (g_max_time < delay)
+                        g_max_time = delay;
                 }
             }
         };
